290915eed8503a9e6666513fde9c026ef4c564cf
linda <lindawilson.may21@gmail.com>
2023-08-24 11:29:36 +0200

    blocking the already booked dates for checkin & checkout

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -6,28 +6,28 @@ export default class extends Controller {
     flatpickr(this.element, {
       "disable": [
         function(date) {
-          console.log(date)
           // Disable if the date is within a specific range (e.g., from @booking.check_in to @booking.check_out)
-          const checkInDate = new Date("2023-08-25");
-          const checkOutDate = new Date("2023-08-28");
-          //console.log(checkInDate)
-          //console.log(checkOutDate)
+          const element = document.getElementById('my-element');
+          const bookings = element.getAttribute('data-all-bookings');
 
-          var element = document.getElementById('my-element');
-          var bookings = element.getAttribute('data-all-bookings');
+          const bookingArray = JSON.parse(bookings);
+          const bookingRange = bookingArray.some((item)=>{
 
-          var jsonArray = JSON.parse(bookings);
-          jsonArray.forEach(function(item) {
-           // console.log(item.check_in);
-          //console.log(item.check_out)
+            const checkIn = new Date(item.check_in)
+            checkIn.setHours(0,0,0)
+            const checkOut = new Date(item.check_out)
+            checkOut.setHours(0,0,0)
+            date.setHours(0,0,0)
 
-            if (date >= item.check_in && date <= item.check_out)
-              return true;
+            return date >= checkIn && date <= checkOut;
           })
 
+          if(bookingRange == true) {
+              return true
+          }
+          
 
-
-          // Disable if the date is in the past
+       // Disable if the date is in the past
           const today = new Date();
           today.setHours(0, 0, 0, 0); // Set time to the start of the day
 
