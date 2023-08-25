332006350933992b68154a3c290bc27476159540
arthur rohr <arthur.rohr.pcc@gmail.com>
2023-08-25 14:58:35 +0200

    add the delete button for jet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Jet.destroy_all
+User.destroy_all
 
-20.times do
-  Jet.create([
-    {
-      name: Faker::Drone.name,
-      model: Faker::Vehicle.vin,
-      capacity: [1..14].sample,
-      jetType: JETTYPE.sample,
-      location: Faker::Address.full_address,
-      price: [10000..100000].sample,
-      user_id: 1
-    }
-  ])
-end
+# 20.times do
+#   Jet.create([
+#     {
+#       name: Faker::Drone.name,
+#       model: Faker::Vehicle.vin,
+#       capacity: [1..14].sample,
+#       jetType: JETTYPE.sample,
+#       location: Faker::Address.full_address,
+#       price: [10000..100000].sample,
+#       user_id: 1
+#     }
+#   ])
+# end
